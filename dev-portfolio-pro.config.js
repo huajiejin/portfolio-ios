b6fd84f61e029cceff454cfe490be3598196ffd2
Jay (Huajie) Jin <jay.jin.henan@hotmail.com>
2024-02-14 16:58:32 -0500

    feat: Add sample config, resume, and avatar for Dev Portfolio Pro

---
+++ dev-portfolio-pro.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  name: "Andy",
+  role: "Software Engineer",
+  email: "andy@example.com",
+  avatar_url: "/sample-avatar.jpg",
+  resume_url: "/sample-resume.pdf",
+  bio:
+    "🚀 Andy: Full-stack Developer, React magician, Node.js ninja, and occasional stand-up coder. Explore my projects and let's make tech magic together!",
+  socials: {
+    github: "https://github.com/change-me",
+    linkedin: "https://www.linkedin.com/in/change-me",
+    x: "https://www.x.com/change-me",
+  },
+  projects: {
+    "project-1": {
+      title: "Project 1",
+      description: "This is a project I worked on.",
+      image_url: "",
+      url: "",
+      github_url: "",
+      live_url: "",
+      tech_stack: ["React", "Node.js", "Express", "MongoDB"],
+    },
+    "project-2": {
+      title: "Project 2",
+      description: "This is another project I worked on.",
+      image_url: "",
+      url: "",
+      github_url: "",
+      live_url: "",
+      tech_stack: ["React", "Node.js", "Express", "MongoDB"],
+    },
+  },
+};
