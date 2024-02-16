d9959a50d58374c430f4d9498bfcba587740523c
Jay (Huajie) Jin <jay.jin.henan@hotmail.com>
2024-02-16 17:28:59 -0500

    perf: Utilize tree shaking on framer-motion to reduce bundle size

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -5,7 +5,9 @@ import config from '../../dev-portfolio-pro.config';
 import { CalendarIcon, ArrowTopRightOnSquareIcon, ArrowRightIcon } from '@heroicons/react/24/solid'
 import Tag from './Tag';
 import NewTabButton from './NewTabButton';
-import { MotionValue, motion, useScroll, useTransform } from 'framer-motion';
+// FIXME: Remove framer-motion, because Parallax Scrolling is relatively simple to
+//  implement, and it's not worth the extra bundle size (26kb after tree shaking)
+import { m as motion, LazyMotion, domAnimation, MotionValue, useScroll, useTransform } from 'framer-motion';
 import { useRef } from 'react';
 
 
@@ -21,68 +23,70 @@ export default function Project({ project, className }: { project: Project, clas
   const y = useParallax(scrollYProgress, 100);
 
   return (
-    <div
-      key={project.name}
-      className={`flex justify-center items-center gap-24 flex-wrap-reverse ${className || ''}`}
-      ref={container}
-      >
-      <div className="max-w-md">
-        <div className="text-sm font-medium text-stone-500">{project.name}</div>
-        <p className="mt-4 text-4xl font-semibold">{project.impact}</p>
-        {
-          project.description && (
-            <p className="mt-4">{project.description}</p>
-          )
-        }
-        <div className="mt-4 flex flex-wrap items-center gap-2">
-          <Tag text={project.date}>
-            <CalendarIcon className="h-4 w-4 -translate-y-[0.5px]" />
-          </Tag>
+    <LazyMotion features={domAnimation}>
+      <div
+        key={project.name}
+        className={`flex justify-center items-center gap-24 flex-wrap-reverse ${className || ''}`}
+        ref={container}
+        >
+        <div className="max-w-md">
+          <div className="text-sm font-medium text-stone-500">{project.name}</div>
+          <p className="mt-4 text-4xl font-semibold">{project.impact}</p>
           {
-            project.tech_stack?.map((tech) => (
-              <Tag key={tech} text={tech} />
-            ))
+            project.description && (
+              <p className="mt-4">{project.description}</p>
+            )
+          }
+          <div className="mt-4 flex flex-wrap items-center gap-2">
+            <Tag text={project.date}>
+              <CalendarIcon className="h-4 w-4 -translate-y-[0.5px]" />
+            </Tag>
+            {
+              project.tech_stack?.map((tech) => (
+                <Tag key={tech} text={tech} />
+              ))
+            }
+          </div>
+          {
+            (project.live_url || project.url) && (
+              <div className="mt-4 flex justify-start items-center gap-2">
+                {
+                  project.live_url && (
+                    <NewTabButton href={project.live_url}>
+                      Live demo
+                      <ArrowTopRightOnSquareIcon className="h-4 w-4" />
+                    </NewTabButton>
+                  )
+                }
+                {
+                  project.url && (
+                    <NewTabButton href={project.url}>
+                      Learn more
+                      <ArrowRightIcon className="h-4 w-4" />
+                    </NewTabButton>
+                  )
+                }
+              </div>
+            )
           }
         </div>
         {
-          (project.live_url || project.url) && (
-            <div className="mt-4 flex justify-start items-center gap-2">
-              {
-                project.live_url && (
-                  <NewTabButton href={project.live_url}>
-                    Live demo
-                    <ArrowTopRightOnSquareIcon className="h-4 w-4" />
-                  </NewTabButton>
-                )
-              }
-              {
-                project.url && (
-                  <NewTabButton href={project.url}>
-                    Learn more
-                    <ArrowRightIcon className="h-4 w-4" />
-                  </NewTabButton>
-                )
-              }
-            </div>
+          project.image_url ? (
+            <motion.div className="max-w-md flex-shrink-0 overflow-hidden rounded-lg shadow-xl" style={{y}}>
+              <Image
+                src={project.image_url}
+                alt={`Image of ${project.name}`}
+                className="w-full h-auto object-cover object-center"
+                width={1080}
+                height={1080}
+                priority
+              />
+            </motion.div>
+          ) : (
+            <div className="max-w-md w-full" />
           )
         }
       </div>
-      {
-        project.image_url ? (
-          <motion.div className="max-w-md flex-shrink-0 overflow-hidden rounded-lg shadow-xl" style={{y}}>
-            <Image
-              src={project.image_url}
-              alt={`Image of ${project.name}`}
-              className="w-full h-auto object-cover object-center"
-              width={1080}
-              height={1080}
-              priority
-            />
-          </motion.div>
-        ) : (
-          <div className="max-w-md w-full" />
-        )
-      }
-    </div>
+    </LazyMotion>
   );
 }
