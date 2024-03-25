5843c271d2d962a6752fad5a7abe19bcafe0a60d
Jay (Huajie) Jin <jay.jin.henan@hotmail.com>
2024-03-25 13:23:38 -0400

    feat: Add plausible analytics script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import config from '../../dev-portfolio-pro.config';
+import Script from 'next/script';
 
 export const metadata: Metadata = {
   title: `${config.name} | ${config.role} - Portfolio`,
@@ -14,6 +15,9 @@ export default function RootLayout({
 }>) {
   return (
     <html className="sm:snap-y sm:snap-mandatory" lang="en">
+      <head>
+        <Script defer data-domain="huajiejin.github.io" src="https://plausible.jinshub.com/js/script.js" />
+      </head>
       <body className="dark-container-1">{children}</body>
     </html>
   );
