3e3de4bc4e546b31a1acf93939b4af3bce19cb3b
Jay (Huajie) Jin <jay.jin.henan@hotmail.com>
2024-03-25 13:04:20 -0400

    chore: Update config

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
   output: "export",
   // basePath should match the repository name if you are deploying to GitHub pages
-  basePath: "/dev-portfolio-pro",
+  basePath: "/portfolio-fe",
   images: {
     unoptimized: true,
   },
